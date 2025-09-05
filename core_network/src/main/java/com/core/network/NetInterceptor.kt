1f4b10d1e2715f5a22e61b91505e4321b9a5906f
villa_mou <850462897@qq.com>
2025-09-05 11:40:36 +0800

    feat: 升级

--- core_network/src/main/java/com/core/network/NetInterceptor.kt
+++ core_network/src/main/java/com/core/network/NetInterceptor.kt
@@ -14,7 +14,6 @@ class NetInterceptor(private val handler: RequestHandler?) : Interceptor {
     /**
      * 网络请求拦截方法
      */
-    @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         if (handler != null) {
--- core_network/src/main/java/com/core/network/provider/BaseNetProvider.kt
+++ core_network/src/main/java/com/core/network/provider/BaseNetProvider.kt
@@ -14,6 +14,9 @@ import com.core.network.api.RequestHandler
 import com.core.network.bean.BaseBean
 import com.core.network.bean.HttpException
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.net.URLDecoder
 import java.nio.charset.Charset
@@ -49,7 +52,7 @@ open class BaseNetProvider(private val context: Context) : NetProvider {
     }
 
     override fun configCookie(): PersistentCookieJar =
-            PersistentCookieJar(SetCookieCache(), SharedPrefsCookiePersistor(context))
+        PersistentCookieJar(SetCookieCache(), SharedPrefsCookiePersistor(context))
 
     override fun configHandler(): RequestHandler = HeaderHandler()
 
@@ -66,40 +69,84 @@ open class BaseNetProvider(private val context: Context) : NetProvider {
 
     inner class HeaderHandler : RequestHandler {
         override fun onBeforeRequest(request: Request, chain: Interceptor.Chain): Request {
-            return if (request.method() == "POST") {
-                if (request.body() is FormBody) {
-                    val mediaType: MediaType? = MediaType.parse("application/json; charset=utf-8")
-                    val jsonObject = JSONObject()
-                    val formBody = request.body() as FormBody
-                    for (index in 0 until formBody.size()) {
-                        if (formBody.encodedValue(index).isNotEmpty() && formBody.encodedName(index).isNotEmpty()) {
-                            jsonObject.put(formBody.encodedName(index), formBody.encodedValue(index))
+            return if (request.method == "POST") {
+                when (val body = request.body) {
+                    is FormBody -> {
+                        val jsonObject = JSONObject()
+                        for (i in 0 until body.size) {
+                            val name = body.name(i)
+                            val value = body.value(i)
+                            if (name.isNotEmpty() && value.isNotEmpty()) {
+                                jsonObject.put(name, value)
+                            }
                         }
+                        val jsonBody = jsonObject.toString()
+                            .toRequestBody("application/json; charset=utf-8".toMediaType())
+
+                        request.newBuilder()
+                            //.header("Connection", "close") // 可选
+                            .post(jsonBody)
+                            .build()
                     }
-                    val requestBody: RequestBody = RequestBody.create(mediaType, URLDecoder.decode(jsonObject.toString(), "UTF-8"))
-                    request.newBuilder()
-                            .header("Connection","close")
-                            .post(requestBody)
-                            .build()
-                } else {
-                    request.newBuilder()
-                            .header("Connection","close")
-                            .post(request.body())
-                            .build()
+
+                    is MultipartBody -> {
+                        // MultipartBody 不做处理，保持原样
+                        request
+                    }
+
+                    else -> {
+                        // 如果已经是 JSON 或其他 RequestBody，保持原样
+                        request
+                    }
                 }
             } else {
                 request
             }
+
+//            return if (request.method == "POST") {
+//                if (request.body is FormBody) {
+//                    val mediaType: MediaType? =
+//                        "application/json; charset=utf-8".toMediaTypeOrNull()
+//                    val jsonObject = JSONObject()
+//                    val formBody = request.body as FormBody
+//                    for (index in 0 until formBody.size) {
+//                        if (formBody.encodedValue(index).isNotEmpty() && formBody.encodedName(index)
+//                                .isNotEmpty()
+//                        ) {
+//                            jsonObject.put(
+//                                formBody.encodedName(index),
+//                                formBody.encodedValue(index)
+//                            )
+//                        }
+//                    }
+//                    val requestBody: RequestBody = RequestBody.create(
+//                        mediaType,
+//                        URLDecoder.decode(jsonObject.toString(), "UTF-8")
+//                    )
+//                    request.newBuilder()
+//                        .header("Connection", "close")
+//                        .post(requestBody)
+//                        .build()
+//                } else {
+//                    request.newBuilder()
+//                        .header("Connection", "close")
+//                        .post(request.body!!)
+//                        .build()
+//                }
+//            } else {
+//                request
+//            }
         }
 
         override fun onAfterRequest(response: Response, chain: Interceptor.Chain): Response {
             if (response.isSuccessful) {
                 //这里不能使用response.body().string()来进行读取
-                val source = response.body()?.source()
+                val source = response.body?.source()
                 source?.request(Long.MAX_VALUE)
                 val buffer = source?.buffer()
                 val body = buffer?.clone()?.readString(Charset.forName("UTF-8"))
-                val bean: BaseBean<Any> = Gson().fromJson(body, object : TypeToken<BaseBean<Any>>() {}.type)
+                val bean: BaseBean<Any> =
+                    Gson().fromJson(body, object : TypeToken<BaseBean<Any>>() {}.type)
 //                if (bean.errorCode != HttpStatusConstants.SUCCESS){
 //                    throw HttpException(bean) // 外部异常，系统维护中
 //                }
